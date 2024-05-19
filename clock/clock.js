b3b2ef37c3a2a85e2432c8ee72c4813a7a1ba152
d3ltah <deltah404@gmail.com>
2024-05-19 16:47:36 +0100

    give the 404 page a little more character

--- clock/clock.js
+++ clock/clock.js
@@ -1,39 +1,37 @@
 function ordinal_suffix_of(i) {
-    let j = i % 10,
-        k = i % 100;
-    if (j === 1 && k !== 11) {
-        return i + "st";
-    }
-    if (j === 2 && k !== 12) {
-        return i + "nd";
-    }
-    if (j === 3 && k !== 13) {
-        return i + "rd";
-    }
-    return i + "th";
+	let j = i % 10,
+		k = i % 100;
+	if (j === 1 && k !== 11) {
+		return i + "st";
+	}
+	if (j === 2 && k !== 12) {
+		return i + "nd";
+	}
+	if (j === 3 && k !== 13) {
+		return i + "rd";
+	}
+	return i + "th";
 }
 
 function updateTimeDisplay() {
-    const time = getCurrentTime();
-    const timeWeekDay = time.toLocaleDateString("en-US", { weekday: "long" });
-    const timeDay = time.toLocaleDateString("en-US", { day: "numeric" });
-    const timeMonth = time.toLocaleDateString("en-US", { month: "long" });
-    const timeYear = time.toLocaleDateString("en-US", { year: "numeric" });
+	const time = getCurrentTime();
+	const timeWeekDay = time.toLocaleDateString("en-US", { weekday: "long" });
+	const timeDay = time.toLocaleDateString("en-US", { day: "numeric" });
+	const timeMonth = time.toLocaleDateString("en-US", { month: "long" });
+	const timeYear = time.toLocaleDateString("en-US", { year: "numeric" });
 
-    const timeHour = ("0" + time.getHours()).slice(-2);
-    const timeMinute = ("0" + time.getMinutes()).slice(-2);
-    const timeSecond = ("0" + time.getSeconds()).slice(-2);
+	const timeHour = ("0" + time.getHours()).slice(-2);
+	const timeMinute = ("0" + time.getMinutes()).slice(-2);
+	const timeSecond = ("0" + time.getSeconds()).slice(-2);
 
+	document.getElementById("time-dow").innerHTML = timeWeekDay;
+	document.getElementById("time-day").innerHTML = ordinal_suffix_of(timeDay);
+	document.getElementById("time-month").innerHTML = timeMonth;
+	document.getElementById("time-year").innerHTML = timeYear;
 
-
-    document.getElementById("time-dow").innerHTML = timeWeekDay;
-    document.getElementById("time-day").innerHTML = ordinal_suffix_of(timeDay);
-    document.getElementById("time-month").innerHTML = timeMonth;
-    document.getElementById("time-year").innerHTML = timeYear;
-
-    document.getElementById("clock-hour").innerHTML = timeHour;
-    document.getElementById("clock-minute").innerHTML = timeMinute;
-    document.getElementById("clock-second").innerHTML = timeSecond;
+	document.getElementById("clock-hour").innerHTML = timeHour;
+	document.getElementById("clock-minute").innerHTML = timeMinute;
+	document.getElementById("clock-second").innerHTML = timeSecond;
 }
 
 setInterval(updateTimeDisplay, 0);
